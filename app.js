37f284454597abd2933b4dfdfdd28ed218f8da4f
Dário Prazeres <darioedgarprazeres@gmail.com>
2022-05-09 23:25:32 +0100

    update, it is a point when i can see all publications

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ const Schema = Mongoose.Schema;
 var bcrypt = require('bcryptjs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var {body, validationResult} = require('express-validator');
+var async =  require('async');
 //conectio with DB
 const mongoDb = 'mongodb://127.0.0.1/myDB'//"mongodb+srv://m001-student:m001-mongodb-basics@cluster0.0cgbl.mongodb.net/user?retryWrites=true&w=majority";
 Mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = Mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
-
+// This section Of code is for Models
 const User = Mongoose.model(
   "User",
   new Schema({
@@ -25,7 +26,14 @@ const User = Mongoose.model(
     password: { type: String, required: true }
   })
 );
-
+const Publication = Mongoose.model(
+  'Publication',
+  new Schema({
+    title: {type: String, required:true},
+    content: { type: String, required: true},
+    user: {type: Schema.Types.ObjectId, ref: 'User', required:true}
+  })
+);
 var app = express();
 
 // view engine setup
@@ -69,7 +77,13 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/', indexRouter);
+app.get('/', function(req, res, next) {
+  Publication.find({}).sort({title: 1}).exec(function(err, result){
+    if(err){return next(err);}
+    res.render('index', { title: 'Home', user: req.user, list_Publication: result});
+  })
+  
+});
 app.use('/users', usersRouter);
 app.get('/sign-up', (req, res) => {
   res.render('sign-up-form')
@@ -94,6 +108,35 @@ app.post(
     failureRedirect: "/"
   })
 );
+app.get('/create-post', (req, res, next) => {
+  const errors = validationResult(req);
+  res.render('create-post', {title: "Create Post", user: req.user, errors: errors.array(), publication: Publication});
+})
+app.post('/create-post', [
+  body('title', 'Title must not be empty.').trim().isLength({min: 1}).escape(),
+  body('content', 'Content must not be empty.').trim().isLength({min: 1}).escape(),
+  
+  (req, res, next) =>{
+    const errors = validationResult(req);
+    var publication = new Publication(
+      {
+        title:req.body.title,
+        content: req.body.content,
+        user: req.body.userID
+      }
+    ); 
+    if(!errors.isEmpty()){
+      res.render('create-post', {title: 'Create Post', publication:publication, errors: errors.array(), user:req.user})
+      return;
+    }
+    else{
+      publication.save(function(err){
+        if(err){return next(err);}
+        res.redirect('/');
+      })
+    }
+  }
+]);
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 module.exports = router;
+/* GET users listing.
+<!DOCTYPE html>
+<html>
+  <head>
+    <%- include('./partials/head'); -%>
+  </head>
+  <body>
+    <header>
+      <%- include('./partials/header.ejs'); -%>
+    </header>
+    <% if (user) {%>
+      <h1>WELCOME BACK <%= user.username %></h1>
+      <a href="/log-out">LOG OUT</a>
+    <% } else { %>
+      <h1>please log in</h1>
+      <form action="/log-in" method="POST">
+        <label for="username">Username</label>
+        <input name="username" placeholder="username" type="text" />
+        <label for="password">Password</label>
+        <input name="password" type="password" />
+        <button class="btn btn-primary">Log In</button>
+      </form>
+    <%}%>
+    <footer>
+      <%- include('./partials/footer.ejs'); -%>
+    </footer>
+  </body>
+</html> */
