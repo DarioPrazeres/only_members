077cfea5daaf66acabc698d85b8ad521b7befe02
Dário Prazeres <darioedgarprazeres@gmail.com>
2022-05-11 10:24:35 +0100

    update app.js

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ var async =  require('async');
 
 
 //require in DOTENV document
-require('dotenv').config();
-console.log(process.env.FOO);
 console.log(process.env.DB_LINK);
 //conectio with DB
 const mongoDb = process.env.DB_LINK;
